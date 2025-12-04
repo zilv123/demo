b7753ba32929a7514c9e6b7d3cd4090d34334e28
Chen <95120083+zilv123@users.noreply.github.com>
2025-12-04 15:27:33 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,25 +7,25 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'v0 App',
+  title: '个人网站',
   description: 'Created with v0',
   generator: 'v0.app',
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: '',
         media: '(prefers-color-scheme: dark)',
       },
       {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
+        url: '',
+        type: '',
       },
     ],
-    apple: '/apple-icon.png',
+    apple: '',
   },
 }
 

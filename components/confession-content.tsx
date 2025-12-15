88565c8781fe87411f78d7ca165c9396f55b207b
Chen <95120083+zilv123@users.noreply.github.com>
2025-12-15 11:49:09 +0800

    Update confession-content.tsx

--- components/confession-content.tsx
+++ components/confession-content.tsx
@@ -8,7 +8,7 @@ export default function ConfessionContent() {
   const [confirmed, setConfirmed] = useState(false)
   const [noCount, setNoCount] = useState(0)
 
-  const fullText = "看什么看？"
+  const fullText = "及你太美！！！"
 
   useEffect(() => {
     if (displayText.length < fullText.length) {
